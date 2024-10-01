1e88e188bffa271660f615aae6d9fe9c79a551e3
Kristina_Polyakova_FPU-115 <kris1617polyakova@yandex.ru>
2024-10-01 21:01:03 +0700

    exercise №4

--- HW_Class.py
+++ HW_Class.py
@@ -19,15 +19,18 @@
 
     def get_gpa(self):
         grades_list = [mark for marks in self.grades.values() for mark in marks]
-        gpa = sum(grades_list) / len(grades_list)
-        return gpa
-
+        if len(grades_list) == 0:
+            return 0
+        else:
+            gpa = sum(grades_list) / len(grades_list)
+            return gpa
+            
     def __str__(self):
         return ('Имя: ' + self.name + '\n' + 
                 'Фамилия: ' + self.surname + '\n' + 
                 'Средняя оценка за домашние задания: ' + str(self.get_gpa()) + 
                 '\n' + 'Курсы в процессе изучения: ' +  ', '.join(self.courses_in_progress) +
-                '\n' + 'Завершенные курсы:' + ', '.join(self.finished_courses))
+                '\n' + 'Завершенные курсы: ' + ', '.join(self.finished_courses))
 
     def __eq__(self, other):
         return self.get_gpa() == other.get_gpa()
@@ -62,8 +65,11 @@ class Lecturer(Mentor):
     
     def get_gpa(self):
         grades_list = [mark for marks in self.grades.values() for mark in marks]
-        gpa = sum(grades_list) / len(grades_list)
-        return gpa
+        if len(grades_list) == 0:
+            return 0
+        else:
+            gpa = sum(grades_list) / len(grades_list)
+            return gpa
 
     def __str__(self):
         return ('Имя: ' + self.name + '\n' + 
@@ -105,3 +111,74 @@ class Reviever(Mentor):
 
     def __str__(self):
         return 'Имя: ' + self.name + '\n' + 'Фамилия: ' + self.surname
+
+
+student1 = Student('Мария','Романова','жен.')
+student2 = Student('Евгений','Семёнов','муж.')
+
+lector1 = Lecturer('Станислав','Комаров')
+lector2 = Lecturer('Оксана','Щукина')
+
+reviever1 = Reviever('Сергей','Возницкий')
+reviever2 = Reviever('Александр','Борисов')
+
+reviever1.courses_attached.append('ООП и работа с API')
+reviever2.courses_attached.append('ООП и работа с API')
+
+lector1.courses_attached.append('ООП и работа с API')
+lector2.courses_attached.append('Основы Python')
+
+student1.courses_in_progress.append('ООП и работа с API')
+student1.courses_in_progress.append('Основы Python')
+student1.finished_courses.append('Как учиться онлайн')
+
+student2.courses_in_progress.append('ООП и работа с API')
+student2.finished_courses.append('Основы Python')
+
+reviever1.rate_hw(student1, 'ООП и работа с API', 6)
+reviever1.rate_hw(student2, 'ООП и работа с API', 8)
+reviever2.rate_hw(student1, 'ООП и работа с API', 7)   
+reviever2.rate_hw(student2, 'ООП и работа с API', 10)
+
+student1.rate_lecturer(lector1,'ООП и работа с API', 9)
+student2.rate_lecturer(lector1,'ООП и работа с API', 6)
+
+average_grade_student1 = student1.get_gpa()
+average_grade_lector1 = lector1.get_gpa()
+
+print(f'СТУДЕНТ \n{student1}')
+print(f'ЛЕКТОР \n{lector1}')
+print(f'ЭКСПЕРТ \n{reviever1}')
+
+print(student1 < student2)
+print(lector1 == lector2)
+
+students_list = [student1, student2]
+course_name = 'ООП и работа с API'
+lecturer_list = [lector1, lector2]
+
+def get_gpa_students(students, course):
+    all_grades = []
+    for student in students:
+        if course in student.grades:
+            grades_list = [marks for marks in student.grades[course]]
+            all_grades += grades_list
+    if len(all_grades) != 0:
+        return sum(all_grades) / len(all_grades)
+    else:
+        return 0
+    
+print(get_gpa_students(students_list, course_name))
+
+def get_gpa_lecturer(lectors, course):
+    all_grades = []
+    for lector in lectors:
+        if course in lector.grades:
+            grades_list = [marks for marks in lector.grades[course]]
+            all_grades += grades_list
+    if len(all_grades) != 0:
+        return sum(all_grades) / len(all_grades)
+    else:
+        return 0
+    
+print(get_gpa_lecturer(lecturer_list, course_name))
